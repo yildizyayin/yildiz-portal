52ecd287a090a17cf669ab78a833039a9f338115
yildizyayin <rtsgida@gmail.com>
2026-08-19 01:11:38 +0300

    Setup: Add Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8787',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist/public',
+    emptyOutDir: true,
+  },
+});
